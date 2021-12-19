82c137d969dbec3fd2b6738df9998bf5d41933ff
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-12-19 12:06:12 -0300

    feat: add eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  globals: {
+    chrome: 'readonly',
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    semi: ['error', 'never'],
+    complexity: ['error', 10],
+    'no-unexpected-multiline': 'error',
+    'func-names': ['error', 'never'],
+    'spaced-comment': ['off'],
+    'no-underscore-dangle': ['off'],
+    'no-unused-vars': ['error'],
+    'no-unused-expressions': ['error'],
+    'linebreak-style': 0,
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-extra-boolean-cast': ['off'],
+    'no-param-reassign': ['off'],
+    'no-use-before-define': 'off',
+    'semi-style': ['error'],
+    indent: ['error', 2],
+    'no-trailing-spaces': ['error'],
+    'no-multi-spaces': 'error',
+    'no-undef': ['error', { typeof: true }],
+    'space-before-function-paren': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'quote-props': ['error', 'as-needed'],
+    'require-jsdoc': ['error', {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: false,
+        ArrowFunctionExpression: false,
+        FunctionExpression: true
+      }
+    }],
+    'valid-jsdoc': ['error', {
+      requireReturnDescription: false
+    }],
+    'space-infix-ops': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+  },
+}
